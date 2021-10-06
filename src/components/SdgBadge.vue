2d90b54ebdd78ed28fcc5943936dc5fac92440dd
Axel Köhler <contact@axelkohler.nl>
2021-10-06 17:04:34 +0200

    Add Solution Excerpt component
    Create a component to represent a single solution in a grid of multiple

---
+++ src/components/SdgBadge.vue
@@ -0,0 +1,26 @@
+<template>
+  <span class="label" :style="{backgroundColor: color}">{{number}}.</span>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import SdgColors from '@/data/SdgColors'
+
+@Component({
+  props: {
+    number: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    color: function () {
+      return SdgColors[this.number - 1]
+    }
+  }
+})
+export default class SolutionExcerpt extends Vue {}
+</script>
+
+<style>
+</style>
