21ab70fdec805f37d6ba239768dc30d3e1c2f0f3
Robin de Witte <r.dewitte@student.fontys.nl>
2021-10-18 14:16:51 +0200

    timesetter met library
    timesetter vue component om tijd van automatisch posten aan te passen, link met backend moet nog

---
+++ src/components/EcoBob/TimeSetter.vue
@@ -0,0 +1,29 @@
++<template>
+    <div onload="GetCurrentTime()">
+        <small>Selecteer hier nieuwe tijd automatisch bericht.</small>	
+        <vue-timepicker v-model="TimeValue" format="HH:mm:ss"></vue-timepicker>
+
+        <small> huidige tijd automatische post:    <script type="text/javascript">
+        document.write(currentTime)
+      </script></small>
+
+        <button (click)="SetTime()">
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class TimeSetter extends Vue {
+    private currentTime: String = "";
+GetCurrentTime(){
+    //get data from service
+    //currentime = data
+}
+SetTime(){
+    //datastuff met TimeValue
+}
+}
+
+</script>
