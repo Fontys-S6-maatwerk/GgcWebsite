02ae99b5b5e55dfb52e5e3bf67dee4bc545b4af5
Robin de Witte <r.dewitte@student.fontys.nl>
2021-11-02 14:31:28 +0100

    aanpassen api url

--- src/services/TwitterbotService.ts
+++ src/services/TwitterbotService.ts
@@ -3,13 +3,13 @@ import http from '@/services/http';
 export default class TwitterbotService{
 
     public async GetCurrentTime(): Promise<String>{
-        const response = await http.get('/api/bot/currenttime');
+        const response = await http.get('/api/twitter/currenttime');
         return response.data;
     }
 
     public async SetCurrentTime(newTime: String){
         console.log(newTime);
-        const response = await http.post('/api/bot/settime', newTime);
+        const response = await http.post('/api/twitter/settime', newTime);
         return response.data;
     }
 }
