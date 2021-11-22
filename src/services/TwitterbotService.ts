b02d88c5732d69aee5a3ab4688e02427b28edab1
Robin de Witte <r.dewitte@student.fontys.nl>
2021-11-22 14:09:13 +0100

    link adjust

--- src/services/TwitterbotService.ts
+++ src/services/TwitterbotService.ts
@@ -3,13 +3,13 @@ import http from '@/services/http';
 export default class TwitterbotService{
 
     public async GetCurrentTime(){
-        const response = await http.get('/api/twitter/currenttime');
+        const response = await http.get('/api/twitter/gettime');
         return response.data;
     }
 
     public async SetCurrentTime(newTime: String){
         console.log(newTime);
-        const response = await http.post('/api/twitter/settime', newTime);
+        const response = await http.post('/api/twitter/adjusttime', newTime);
         return response.data;
     }
 }
